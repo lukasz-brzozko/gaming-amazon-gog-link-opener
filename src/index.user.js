f036f5180d1383d2865db20d9f85cbc47b1bd4a0
Łukasz Brzózko <lukasz9099@gmail.com>
2024-07-07 14:27:04 +0200

    config: update meta data

--- src/index.user.js
+++ src/index.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
-// @name         Gog Redeem Opener
+// @name         GOG Redeem Opener
 // @namespace    http://tampermonkey.net/
 // @version      2024-07-05
-// @description  try to take over the world!
-// @author       You
+// @description  Opens the GOG redeem page with the filled code input
+// @author       Łukasz Brzózko
 // @match        https://gaming.amazon.com/*
 // @icon         https://m.media-amazon.com/images/G/01/samus/prime/Favicon_72x72.svg
+// @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/gaming-amazon-gog-link-opener/main/dist/index.meta.js
+// @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/gaming-amazon-gog-link-opener/main/dist/index.user.js
 // @grant        none
 // ==/UserScript==
 
